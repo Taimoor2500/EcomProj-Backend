d121f78b61313bf19769e0aba01e03e78382cf43
Taimoor2500 <taimoorrashid858@gmail.com>
2023-07-30 12:12:06 +0500

    added admin product routes

--- Routes/admin/products.js
+++ Routes/admin/products.js
@@ -62,4 +62,74 @@ router.get('/', async (req, res) => {
     res.status(500).json({ error: 'Internal server error' });
   }
 });
+
+router.put('/:id', upload.single('image'), async (req, res) => {
+  try {
+    const productId = req.params.id;
+    const { title, description, price, color, stock } = req.body;
+    const image = req.file ? `http://localhost:5000/images/${req.file.filename}` : undefined;
+
+    // Find the product by ID
+    const product = await Product.findById(productId);
+
+    if (!product) {
+      return res.status(404).json({ message: 'Product not found' });
+    }
+
+    // Update the product with the provided fields
+    if (title) {
+      product.title = title;
+    }
+
+    if (description) {
+      product.description = description;
+    }
+
+    if (price) {
+      product.price = price;
+    }
+
+    if (color) {
+      product.color = color;
+    }
+
+    if (stock) {
+      product.stock = stock;
+    }
+
+    if (image) {
+      product.image = image;
+    }
+
+    // Save the updated product
+    const updatedProduct = await product.save();
+
+    return res.status(200).json(updatedProduct);
+  } catch (error) {
+    console.error('Error updating product:', error);
+    return res.status(500).json({ message: 'An error occurred while updating the product' });
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const productId = req.params.id;
+console.log(productId);
+    
+    const product = await Product.findById(productId);
+
+    if (!product) {
+      return res.status(404).json({ message: 'Product not found' });
+    }
+
+    
+    await product.deleteOne();
+
+    res.status(200).json({ message: 'Product deleted successfully' });
+  } catch (error) {
+    console.error('Error deleting product:', error);
+    res.status(500).json({ message: 'An error occurred while deleting the product' });
+  }
+});
+
 module.exports = router;
